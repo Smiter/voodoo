a300d31af488fd49017a49951832121e7bc12b8d
Admin <smiter@ubuntu.(none)>
2012-11-27 21:43:50 +0200

    added localization for django admin widgets

--- voodoo/urls.py
+++ voodoo/urls.py
@@ -7,11 +7,14 @@ admin.autodiscover()
 # from registration.models import RegistrationProfile
 # admin.site.unregister(RegistrationProfile)
 # from django.contrib import admin
+js_info_dict = {'packages': ('django.conf', 'django.contrib.admin',), }
+
 
 urlpatterns = patterns('',
     ('^index/$', index),
     ('^login/$', login),
-    (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog'),
+    # (r'^admin/jsi18n/', 'django.views.i18n.javascript_catalog'),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', js_info_dict),
     ('^notice_of_payment/$', notice_of_payment),
     url(r'^captcha/', include('captcha.urls')),
     url(r'^admin/', include(admin.site.urls)),
