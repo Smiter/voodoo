8aba3e32883faa8f3f2145e7016763e3c8c5d67d
Gosteg <igulyanitsky@gmail.com>
2012-12-01 18:47:56 +0200

    Added 'database host' to settings.py. Windows POWER.

--- voodoo/settings.py
+++ voodoo/settings.py
@@ -15,8 +15,8 @@ DATABASES = {
         'NAME': 'django_db',                      # Or path to database file if using sqlite3.
         'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '111',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
