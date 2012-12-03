696eb48768987971cfcc4ea3024a52bd650408a0
Admin <smiter@ubuntu.(none)>
2012-12-03 23:56:41 +0200

    removed login logout pages and smaal change in urls

--- voodoo/settings.py
+++ voodoo/settings.py
@@ -182,7 +182,7 @@ INSTALLED_APPS = (
 )
 
 ACCOUNT_ACTIVATION_DAYS = 7 # One-week activation window; you may, of course, use a different value.
-LOGIN_REDIRECT_URL='/index/'
+#LOGIN_REDIRECT_URL='/index/'
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
