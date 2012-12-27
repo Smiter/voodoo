a106d901e93b2d37f0921e3e025e0afe32f20d31
Igor <igulyanitsky@gmail.com>
2012-12-28 01:44:18 +0700

    updated encoding

--- voodoo/files/sql/23-12-12.sql
+++ voodoo/files/sql/23-12-12.sql
@@ -1,7 +1,7 @@
 INSERT INTO `admin_center_menu` (`id`, `name`, `title`, `enabled`) VALUES 
-  (1,'order_create','�������� ������',1),
-  (2,'orders_management','���������� ��������',1),
-  (3,'orders_import','������ �������',1),
-  (4,'xls_import','Xls ������',1);
+  (1,'order_create','Создание заказа',1),
+  (2,'orders_management','Менеджмент заказов',1),
+  (3,'orders_import','Импорт заказов',1),
+  (4,'xls_import','Xls импорт',1);
   (5,'service','Сервис',1);
 COMMIT;
