03728c99e162b8c14d632a9b036156e8e7741daf
Gosteg <igulyanitsky@gmail.com>
2013-02-27 11:54:28 +0200

    Order Create/Edit select boxes with statuses are collored now

---
+++ voodoo/admin_center/static/js/topparts.selects.colorable.js
@@ -0,0 +1,46 @@
+function makeElementColorable(el) {
+	if ($(el).val() == 'Сообщен') {
+		$(el).css({'color': 'black', 'font-weight':'bold' });
+	}
+	
+	if ($(el).val() == 'Оформлен') {
+		$(el).css({'color': 'orange', 'font-weight':'bold'});
+	}
+	
+	if ($(el).val() == 'Заказан') {
+		$(el).css({'color': 'green', 'font-weight':'bold'});
+	}
+	
+	if ($(el).val() == 'Доставлен') {
+		$(el).css({'color': 'blue', 'font-weight':'bold'});
+	}
+	
+	if ($(el).val() == 'Отказ') {
+		$(el).css({'color': 'red', 'font-weight':'bold'});
+	}
+}
+
+function makeOrderItemStatusesColorable(select) {
+	select.each(function() {
+		makeElementColorable(this);
+	});
+}
+
+function makeOrderStatusSelectElementColorable(el) {
+	if ($(el).val() == 1 || $(el).text() == 'Принят') {
+		$(el).css({'color': 'black', 'font-weight':'bold'});
+	}
+	
+	if ($(el).val() == 2 ||$(el).text() == 'Обработан') {
+		$(el).css({'color': 'green', 'font-weight':'bold'});
+	}
+	
+	if ($(el).val() == 3 ||$(el).text() == 'Закрыт') {
+		$(el).css({'color': 'red', 'font-weight':'bold'});
+	}
+}
+
+function makeOrderStatusSelectColorable(select) {
+	select.each(function() {
+		makeOrderStatusSelectElementColorable(this);	});
+}
