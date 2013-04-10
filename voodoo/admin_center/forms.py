29a025800d537299fee1d5d299d0adf2850f7186
unknown <IGulyanitskiy@.kiev.luxoft.com>
2013-04-10 10:30:33 +0300

    Admin center. Order Create/Edit. Width for inputs extended.

--- voodoo/admin_center/forms.py
+++ voodoo/admin_center/forms.py
@@ -16,16 +16,16 @@ class OrderForm(forms.ModelForm):
         widgets = {
             'client_additional_information': forms.Textarea(
             attrs={'style': 'max-height:60px;min-height:60px;'
-                  + 'max-width:550px;min-width:550px'}),
+                  + 'max-width:590px;min-width:590px'}),
             'order_info': forms.Textarea(
             attrs={'style': 'max-height:60px;min-height:60px;'
-                  + 'max-width:550px;min-width:550px'}),
+                  + 'max-width:590px;min-width:590px'}),
             'car_additional_information': forms.Textarea(
             attrs={'style': 'max-height:60px;min-height:60px;'
-                  + 'max-width:550px;min-width:550px'}),
+                  + 'max-width:590px;min-width:590px'}),
             'order_additional_information': forms.Textarea(
             attrs={'style': 'max-height:60px;min-height:60px;'
-                  + 'max-width:550px;min-width:550px'}),
+                  + 'max-width:590px;min-width:590px'}),
 #            'order_status': forms.ChoiceField(),
             #'car_vin': TextInput(attrs={'size': '17', 'minlength': '17', 'maxlength': '17'}),
 #            'city_recipient': TextInput(attrs={'size': '63', 'maxlength': '63'}),
